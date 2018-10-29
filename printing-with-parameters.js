6ee8df880ba16f92dc6c5fa88ec07d37b077a477
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-29 18:17:20 +0000

    Third Step is done again

--- printing-with-parameters.js
+++ printing-with-parameters.js
@@ -1,1 +1,31 @@
-printing-with-parameters.js
+var https = require('https');
+
+function getAndPrintHTML (requestParameters) {
+
+  var requestOptions = requestParameters;
+
+  var buffer = "";
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buffer += data;
+
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete:' + buffer);
+    });
+
+  });
+}
+
+var userOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(userOptions);
+
